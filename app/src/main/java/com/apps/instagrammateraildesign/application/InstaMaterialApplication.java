de0c32ee95b33b1eb0136ecb5d4e8c949b61b9cd
mohamedghebeji@gmail.com <mohamedghebeji@gmail.com>
2015-02-01 17:29:08 +0100

    fix bug ND

--- app/src/main/java/com/apps/instagrammateraildesign/application/InstaMaterialApplication.java
+++ app/src/main/java/com/apps/instagrammateraildesign/application/InstaMaterialApplication.java
@@ -2,6 +2,8 @@ package com.apps.instagrammateraildesign.application;
 
 import android.app.Application;
 
+import timber.log.Timber;
+
 /**
  * Created by Mohamed on 29/01/15.
  */
@@ -10,5 +12,6 @@ public class InstaMaterialApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Timber.plant(new Timber.DebugTree());
     }
 }
